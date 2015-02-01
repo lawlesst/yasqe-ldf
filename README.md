aea9df9c8ea3e95ad545ed93d47ef011c1ed9310
Ted Lawless <lawlesst@gmail.com>
2015-02-01 18:11:51 -0500

    Initial working query editor.

---
+++ README.md
@@ -0,0 +1,3 @@
+##LDF query client with YASQE
+
+A demo/experiment of querying a [Linked Data Fragments](http://linkeddatafragments.org/) server with the LDF browser client and the [YASQE](http://yasqe.yasgui.org/) SPARQL query editor.  See those projects for more details.
---
+++ init.js
@@ -0,0 +1,46 @@
+//var startFragment = "http://fragments.dbpedia.org/2014/en";
+var startFragment = "http://ldf-vivo.herokuapp.com/orgref";
+// Add log lines to the log element
+var logger = new ldf.Logger();
+logger._print = function (items) { $('#status').toggle(); };
+
+YASQE.defaults.sparql.showQueryButton = true;
+YASQE.defaults.sparql.callbacks.success =  function(data){console.log("success", data);};
+YASQE.defaults.createShareLink = false;
+YASQE.executeQuery = function(yasqe, callbackOrConfig) {
+    if (yasqe.getQueryType() != 'SELECT') {
+      alert("Only SELECT queries supported at this time.");
+      return
+    }
+    var $status = $('#status');
+    //clear previous results
+    $('.results-box').empty();
+    $status.html("Query running..")
+    query = yasqe.getValue();
+    var fragmentsClient = new ldf.FragmentsClient(startFragment, {'logger': logger});
+    results = new ldf.SparqlIterator(query, { fragmentsClient: fragmentsClient});
+    results.on('data', function(chunk) {
+        //console.log(JSON.stringify(chunk));
+        formatRow(chunk);
+    });
+    results.on('end', function() {
+      $status.text("Query finished.").show();
+    });
+};
+
+//finally, initialize YASQE
+var yasqe = YASQE(document.getElementById("yasqe"));
+
+//add results row
+function formatRow(row) {
+  console.log(row);
+  var $area = $('.results-box');
+  Object.keys(row).forEach(function(key) {
+    var val = row[key];
+    if (val.lastIndexOf("http", 0) == 0) {
+        val = "<a href=\"" + val + "\" target=\"_blank\">" + val + "<a/>";
+    }
+    $area.append(key).append(": ").append(val).append('\n');
+  });
+  $area.append("\n");
+}
